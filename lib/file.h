2fc7821dc4cbb9b10ec853a5a43cd2e43821d300
Nicolas Brousse <nicolas@brousse.info>
2010-09-20 01:54:06 -0700

    export ganglia svn trunk r2340

---
+++ lib/file.h
@@ -0,0 +1,31 @@
+#ifndef FILE_H
+#define FILE_H 1
+
+#include <sys/time.h>
+
+/* Never changes */
+#ifndef BUFFSIZE
+#define BUFFSIZE 4096
+#endif
+
+typedef struct {
+  struct timeval last_read;
+  float thresh;
+  char *name;
+  char *buffer;
+  size_t buffersize;
+} timely_file;
+
+/* FreeBSD seems to gag on these.. Yet still works when not compiled in */
+#if defined(BSD)
+ssize_t readn (int fd, void *vptr, size_t n);
+ssize_t writen (int fd, const void *vptr, size_t n);
+#endif
+int slurpfile (char *filename, char **buffer, int buflen);
+float timediff (const struct timeval *thistime,
+                const struct timeval *lasttime);
+char *update_file(timely_file *tf);
+char *skip_whitespace (const char *p);
+char *skip_token (const char *p);
+
+#endif
